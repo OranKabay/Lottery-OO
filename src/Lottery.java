9360054ef889077ef174950ea127c365dee92799
Oran <oran@emetrix.biz>
2021-01-18 16:02:24 +0000

    OO method fin

--- src/Lottery.java
+++ src/Lottery.java
@@ -1,5 +1,68 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Lottery {
+
+    static ArrayList<Piece> bigSeq = new ArrayList<Piece>();
+    static ArrayList<Piece> lowSeq = new ArrayList<Piece>();
+
+    static List<Integer> bigSeqVals = new ArrayList<Integer>();
+    static List<Integer> lowSeqVals = new ArrayList<Integer>();
+
+    static Set<Integer> bigSet;
+    static Set<Integer> lowSet;
+
     public static void main(String[] args) throws Exception {
-       
+
+        for (int k = 0; k < 210; k++) {
+
+            generateNum();
+
+            bigSet = new HashSet<Integer>(bigSeqVals);
+            lowSet = new HashSet<Integer>(lowSeqVals);
+
+            if (bigSet.size() == bigSeqVals.size() 
+            && lowSet.size() == lowSeqVals.size()) {
+
+                System.out.println(bigSeq + " / " + lowSeq);
+            } 
+            else {
+                System.out.println("DUPLICATE FOUND: " + bigSeq + " / " + lowSeq);
+            }
+
+            deinitialise();
+        }
+
+    }
+
+    public static void generateNum() {
+
+        for(int i = 0; i < 5; i++) {
+            bigSeq.add(new Piece(50));
+        }
+        
+        for(int j = 0; j < 2; j++) {
+            lowSeq.add(new Piece(12));
+        };
+
+        for (Piece bigs : bigSeq) {
+            bigSeqVals.add(bigs.get_val());
+        }
+
+        for (Piece lows : lowSeq) {
+            lowSeqVals.add(lows.get_val());
+        }
+
+    }
+
+    public static void deinitialise(){
+        bigSeq.clear();
+        bigSeqVals.clear();
+        bigSet.clear();
+        lowSeq.clear();
+        lowSeqVals.clear();
+        lowSet.clear();
     }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,3 +1,26 @@
+import java.util.Random;
+
 public class Piece {
-    
+    private int _val;
+
+    public int get_val() {
+        return _val;
+    }
+
+    public void set_val(int _val) {
+        this._val = _val;
+    }
+
+    public Piece() {
+    }
+
+    public Piece(int _maxVal) {
+        Random rand = new Random();
+        this._val = rand.nextInt(_maxVal) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return _val + " ";
+    }
 }
